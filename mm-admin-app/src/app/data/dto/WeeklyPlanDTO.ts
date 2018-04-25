daea2a96e6074539cdce2502e4a67b8fb40ed73c
Jared Sieling <jared.sieling@gmail.com>
2018-04-25 09:31:57 -0500

    Add user ID.

--- mm-admin-app/src/app/data/dto/WeeklyPlanDTO.ts
+++ mm-admin-app/src/app/data/dto/WeeklyPlanDTO.ts
@@ -1,6 +1,7 @@
 import { CustomShoppingItemDTO } from "./CustomShoppingItemDTO";
 
 export class WeeklyPlanDTO {
+    userId: string;
     weekNumber: number;
     mealIds: string[];
     customShoppingItems: CustomShoppingItemDTO[];
