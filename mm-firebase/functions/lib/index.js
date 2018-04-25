73b3cd1190342a7bd30b3ed1729676aa093a4619
Sebastian Zarzycki <sebastian.zarzycki@apzumi.pl>
2018-04-25 23:15:14 +0200

    some experiments around functions (sdk calls, http calls, cors)

--- mm-firebase/functions/lib/index.js
+++ mm-firebase/functions/lib/index.js
@@ -2,15 +2,42 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const cors = require("cors");
+const corsHandler = cors({
+    origin: true
+});
 admin.initializeApp(functions.config().firebase);
+// this handles SDK calls as well as direct HTTP calls
 exports.addTest = functions.https.onRequest((req, res) => {
-    const original = req.query.text;
-    admin.firestore().collection('test').add({ "text": original })
+    return corsHandler(req, res, () => {
+        let newText = '';
+        // SDK call passing data json object with properties
+        if (req.body.data && req.body.data.text) {
+            newText = req.body.data.text;
+        }
+        // direct HTTP call passing data in http query parameter
+        else if (req.query.text) {
+            newText = req.query.text;
+        }
+        admin.firestore().collection('test').add({ 'text': newText })
+            .then((snapshot) => {
+            // you should return json object with data property and your result inside if you want to conform to Firebase SDK protocol
+            res.status(200).json({ data: { result: 'OK', snapshot: snapshot } });
+        })
+            .catch(() => {
+            res.sendStatus(500);
+        });
+    });
+});
+// this handles SDK calls only, context already provides current auth state
+exports.addTest2 = functions.https.onCall((data, context) => {
+    let newText = data.text;
+    return admin.firestore().collection('test').add({ 'text': newText })
         .then((snapshot) => {
-        res.status(200).send(snapshot);
+        return { result: 'OK', snapshot: snapshot };
     })
         .catch(() => {
-        res.status(400).send();
+        return { result: 'ERROR' };
     });
 });
 //# sourceMappingURL=index.js.map
