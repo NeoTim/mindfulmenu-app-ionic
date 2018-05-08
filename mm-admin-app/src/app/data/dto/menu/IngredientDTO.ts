9e6bc8680e662e4995cb82080359d2e30a0840a5
Sebastian Zarzycki <sebastian.zarzycki@apzumi.pl>
2018-05-08 19:27:49 +0200

    small cleanup

--- mm-admin-app/src/app/data/dto/menu/IngredientDTO.ts
+++ mm-admin-app/src/app/data/dto/menu/IngredientDTO.ts
@@ -1,17 +1,18 @@
 import { IdentifiableDTO } from '../IdentifiableDTO';
-import { IngredientCategory } from '../../enum/IngredientCategory';
+import { IngredientCategory } from '../../enum/menu/IngredientCategory';
 
 export class IngredientDTO extends IdentifiableDTO {
 
   /**
    * Ingredients will either have a mealId OR weeklyPlanId (if isCustomItem = true).
    */
+
   mealId: string;
 
   weeklyPlanId: string;
 
   isCustomItem: boolean;
-  
+
   amount: number;
 
   unit: string;
