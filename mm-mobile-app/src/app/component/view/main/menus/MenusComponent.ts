a44fa6e605845438f6ffec7337b6e5b08664d197
Sebastian Zarzycki <sebastian.zarzycki@apzumi.pl>
2018-05-09 10:55:07 +0200

    loading data for menus screen in mobile

--- mm-mobile-app/src/app/component/view/main/menus/MenusComponent.ts
+++ mm-mobile-app/src/app/component/view/main/menus/MenusComponent.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { WeeklyMenuModel } from "../../../../model/WeeklyMenuModel";
 import { WeeklyMenuDTO } from "../../../../data/dto/menu/WeeklyMenuDTO";
-import { classToPlain } from "class-transformer";
 import { MealModel } from "../../../../model/MealModel";
 import { MealDTO } from "../../../../data/dto/menu/MealDTO";
-import { WeeklyMenu } from "../../../../data/local/menu/WeeklyMenu";
+import { UserDTO } from "../../../../data/dto/user/UserDTO";
+import { UserModel } from "../../../../model/UserModel";
+import * as _ from "lodash";
 
 @Component({
   selector: 'menus',
@@ -13,11 +14,21 @@ import { WeeklyMenu } from "../../../../data/local/menu/WeeklyMenu";
 })
 export class MenusComponent {
 
-  weeklyMenu: WeeklyMenu;
+  weeklyMenu: WeeklyMenuDTO;
+  previousWeeklyMenus: WeeklyMenuDTO[];
+
+  favoriteMeals: MealDTO[];
+
+  public currentUser: UserDTO;
+
+  public static readonly WEEK_RANGE: number = 6;
 
   constructor(public navCtrl: NavController,
               public weeklyMenuModel: WeeklyMenuModel,
-              public mealModel: MealModel) {
+              public mealModel: MealModel,
+              public userModel: UserModel) {
+
+    this.currentUser = userModel.currentUser;
   }
 
   ionViewDidLoad() {
@@ -25,24 +36,29 @@ export class MenusComponent {
   }
 
   init() {
-    this.weeklyMenuModel.getWeeklyMenuInRelationToCurrent(-1)
+    this.weeklyMenuModel.getCurrentWeeklyMenu()
       .then((weeklyMenu: WeeklyMenuDTO) => {
-        this.mealModel.getMeals(weeklyMenu.mealIds)
-          .then((meals: MealDTO[]) => {
-            let weeklyMenuWithMeals: WeeklyMenu = WeeklyMenu.fromDTO(weeklyMenu);
-            weeklyMenuWithMeals.meals = meals;
+        this.weeklyMenu = weeklyMenu;
+      })
+      .catch((error) => {});
 
-            this.weeklyMenu = weeklyMenuWithMeals;
+    this.previousWeeklyMenus = [];
 
-            let wm2: WeeklyMenuDTO = WeeklyMenu.toDTO(weeklyMenuWithMeals);
-            console.log('weekly menu DTO converted back from local weekly menu');
-            console.log(wm2);
+    for (let i = -1; i > (-1 - MenusComponent.WEEK_RANGE); i--) {
+      this.weeklyMenuModel.getWeeklyMenuInRelationToCurrent(i)
+        .then((weeklyMenu: WeeklyMenuDTO) => {
+          this.previousWeeklyMenus.push(weeklyMenu);
 
-            let plain = classToPlain(wm2);
-            console.log('weekly menu DTO before send to server');
-            console.log(plain);
-          })
-          .catch((error) => {});
+          if (this.previousWeeklyMenus.length === MenusComponent.WEEK_RANGE) {
+            this.previousWeeklyMenus = _.orderBy(_.compact(this.previousWeeklyMenus), ['weekNumber'], ['desc']);
+          }
+        })
+        .catch((error) => {});
+    }
+
+    this.mealModel.getMeals(this.currentUser.favoriteMealIds)
+      .then((meals: MealDTO[]) => {
+        this.favoriteMeals = meals;
       })
       .catch((error) => {});
   }
