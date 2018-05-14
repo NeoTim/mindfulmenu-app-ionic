54a0ea7f4f6f13a01464c6dd2b8099580588cfd5
Jared Sieling <jared.sieling@gmail.com>
2018-05-14 15:29:30 -0500

    Add some top-notch security to Admin Dashboard.

--- mm-admin-app/src/app/component/view/preliminary/auth/AuthLoginComponent.ts
+++ mm-admin-app/src/app/component/view/preliminary/auth/AuthLoginComponent.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { State } from '../../../../common/State';
+import { StateService } from '@uirouter/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
     selector: 'auth-login',
@@ -8,11 +11,27 @@ import { State } from '../../../../common/State';
 })
 export class AuthLoginComponent implements OnInit {
 
-  constructor() {
-    console.log("constructor");
+  @ViewChild('loginForm')
+  private loginForm: NgForm;
+
+  private loginCredentials = {
+    email: null,
+    password: null
+  };
+
+  constructor(private stateService: StateService,
+      public toastrService: ToastrService) {
   }
 
   ngOnInit() {
-    console.log("on init");
+  }
+
+  submit() {
+    if(this.loginCredentials.email == "admin" && this.loginCredentials.password == "MindfulMenu") {
+      this.stateService.go(State.MAIN.HOME);
+    } else {
+      this.toastrService.error("We didn't find an account with that email and password. Please try again.", "Whoops...");
+    }
+    
   }
 }
