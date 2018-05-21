b6ad958cb8876689766dd9b249f5df8eeca71c4d
Jared Sieling <jared.sieling@gmail.com>
2018-05-21 11:52:37 -0500

    Find user by document ID, which is auth.uid.

--- mm-firebase/functions/src/index.ts
+++ mm-firebase/functions/src/index.ts
@@ -33,30 +33,25 @@ export const syncLoggedUser = functions.https.onCall((data: any, context: Callab
 
 	this.getUserByUid(context.auth.uid)
 		.then((user) => {
+
+			// admin.firestore().collection('users').doc(uid).set({ lastAccessed: new Date() }, { merge: true })
+			// 	.then((result) => { return })
+			// 	.catch((error) => { return })
+			
 			return { user: user };
 		}).catch((error) => {
 			return { error: error };
 		})
 });
 
-const getUserByUid = function (uid: String): Promise<UserDTO> {
+const getUserByUid = function (uid: string): Promise<UserDTO> {
 	return new Promise((resolve, reject) => {
-		admin.firestore().collection('users').where('UID', '==', uid)
-			.limit(1).get()
-			.then((querySnapshot) => {
-				if (querySnapshot.docs.length === 1) {
-					let user = querySnapshot.docs[0].data();
-					console.log('User: ' + user.firstName + ' ' + user.lastName);
+		admin.firestore().collection('users').doc(uid).get()
+			.then((docSnapshot) => {
+				let user = docSnapshot.data();
+				console.log('User: ' + user.firstName + ' ' + user.lastName);
 
-					resolve(plainToClass(UserDTO, user as object));
-
-					admin.firestore().collection('users').doc(querySnapshot.docs[0].id).set({ lastAccessed: new Date() }, { merge: true })
-						.then((result) => { return })
-						.catch((error) => { return })
-				} else {
-					console.error('There are multiple users with the same UID: ' + uid);
-					reject(new Error('There is more than 1 user with this UID.'));
-				}
+				resolve(plainToClass(UserDTO, user as object));
 			}).catch(error => {
 				console.error(error);
 				reject(error);
