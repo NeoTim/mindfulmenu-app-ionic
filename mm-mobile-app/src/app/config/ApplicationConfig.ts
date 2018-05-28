0b9a03dc490aba53649e52e71f62b249f121cbe5
Jared Sieling <jared.sieling@gmail.com>
2018-05-28 09:18:21 -0500

    Release 0.9.12

--- mm-mobile-app/src/app/config/ApplicationConfig.ts
+++ mm-mobile-app/src/app/config/ApplicationConfig.ts
@@ -7,7 +7,7 @@ export class ApplicationConfig {
 
     public applicationOwner: string = 'OurMindfulMenu';
     public applicationName: string = 'OurMindfulMenu';
-    public version: string = '0.9.12 (2018-05-27)';
+    public version: string = '0.9.12 (2018-05-28)';
 
     public production: boolean;
     public firebase: FirebaseConfig;
