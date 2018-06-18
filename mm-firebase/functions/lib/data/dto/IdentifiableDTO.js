17215b85a0c491256c10a050ca4c638183a33897
Sebastian Zarzycki <sebastian.zarzycki@apzumi.pl>
2018-06-18 20:17:22 +0200

    remove ignored files from repo (stop tracking)

--- mm-firebase/functions/lib/data/dto/IdentifiableDTO.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class IdentifiableDTO {
-}
-exports.IdentifiableDTO = IdentifiableDTO;
-//# sourceMappingURL=IdentifiableDTO.js.map
